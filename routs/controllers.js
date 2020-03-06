63f657903d2afac9bc95bb911fce585120834465
rakeshkumar.biswas <rakeshkumar.biswas@webskitters.com>
2020-03-06 19:11:56 +0530

    changed

--- routs/controllers.js
+++ routs/controllers.js
@@ -13,13 +13,13 @@ module.exports = class Controller{
 
    //File upload.
   file(req, res, next){
-      const file = req.file
-      if (!file) {
-        const error = new Error('Please upload a file')
-        error.httpStatusCode = 400
-        return next(error)
-        }
-        res.send(file)
+      // const file = req.file
+      // if (!file) {
+      //   const error = new Error('Please upload a file')
+      //   error.httpStatusCode = 400
+      //   return next(error)
+      //   }
+        res.send(req);
       };
 
     //Get products
